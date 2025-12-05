dd49d713901356bd99aa9934fb206c49c64ef0bf
joldasbaev222 <joldasbaevdauranbek54@gmail.com>
2025-12-05 10:21:22 +0500

    Bot githubqa salindi

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+token = os.getenv("8226534324:AAH5PMaaIoqBZTELTMrNZJ77ZMqcX_Lklj8")
+admin_ids = {7075258047, 2022851663}
---
+++ main.py
@@ -0,0 +1,17 @@
+from aiogram import Bot, Dispatcher
+from config import token
+import asyncio
+from routers import router
+
+my_bot = Bot('8226534324:AAH5PMaaIoqBZTELTMrNZJ77ZMqcX_Lklj8')
+dp = Dispatcher()
+
+
+async def main():
+    print("I am starting ...")
+    dp.include_routers(router)
+    await dp.start_polling(my_bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
