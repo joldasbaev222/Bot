3ea5a0854c19dfbd21e4b98e41ee7c50bc8eac78
joldasbaev222 <joldasbaevdauranbek54@gmail.com>
2025-12-11 19:05:05 +0500

    helloooo

--- db.py
+++ db.py
@@ -1,24 +1,18 @@
 import sqlite3
 
-conn = sqlite3.connect("jd.db")
+conn = sqlite3.connect("my_database.db")
 cursor = conn.cursor()
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    phone TEXT NOT NULL
-)
-""")
-
-# 5 ta odam qoʻshamiz (agar bo'lmasa)
-cursor.execute("INSERT INTO users (name, phone) VALUES (?, ?)", ("Alisher", "+998901112233"))
-cursor.execute("INSERT INTO users (name, phone) VALUES ('Azizbek', '+998907778899')")
-cursor.execute("INSERT INTO users (name, phone) VALUES ('Azamat', '+998935554433')")
-cursor.execute("INSERT INTO users (name, phone) VALUES ('Gulnaz', '+998933336677')")
-cursor.execute("INSERT INTO users (name, phone) VALUES ('Malika', '+998909991122')")
-
+# cursor.execute(
+#     """
+# CREATE TABLE IF NOT EXISTS users (
+# id INTEGER PRIMARY KEY AUTOINCREMENT,
+# username TEXT NOT NULL UNIQUE
+# );
+# """
+# )
+cursor.execute("DELETE FROM users WHERE id=1")
+print("Запись с id=3 успешно удалена")
 conn.commit()
-
-
-print("Bazaģa 5 adam qosildi!")
+# print("Таблица 'users' успешно создана (или уже существовала).")
+# db.close()
